03d3aa002d0293f1159495e1a756d2363c052e58
Yucheng Lin <Pike96@users.noreply.github.com>
2017-04-13 20:00:48 -0700

    Corrected using notes

--- Q1.m
+++ Q1.m
@@ -1,4 +1,6 @@
-mu = [1; 2; 3];
+clear; close all;
+mu = [1 2 3];
 sigma = [3 -1 1; -1 5 3; 1 3 4];
-r = mvnrnd(mu,sigma,10000);
-plot(r(:,1),r(:,2),'+')
+z = [normrnd(0,1) normrnd(0,1) normrnd(0,1)];
+A = chol(sigma,'lower');
+x = (A*z' + mu')';
